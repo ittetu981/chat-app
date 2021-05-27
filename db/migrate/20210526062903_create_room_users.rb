d3ce20bf147d58bdda78dcf051a465377c9acddd
ittetu981 <84568432+ittetu981@users.noreply.github.com>
2021-05-27 11:49:48 +0900

    メッセージの表示

--- db/migrate/20210526062903_create_room_users.rb
+++ db/migrate/20210526062903_create_room_users.rb
@@ -1,7 +1,6 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
-      t.string  :content
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
